2d84e62baf848d002e04a56481ac6a41c9986bb1
Petteri Kaikkonen <petteri.kaikkonen@gmail.com>
2019-01-04 08:47:29 +0200

    Update configuration.js

--- js/configuration.js
+++ js/configuration.js
@@ -1,7 +1,7 @@
 const config = {
   CLIENT_ID: "change-this.apps.googleusercontent.com", // Client ID from the Google Developer Console
   API_KEY: "change-this", // API key from the Google Developer Console
-  DISCOVERY_DOCS: ["https://sheets.googleapis.com/$discovery/rest?version=v4"], // Array of API discovery doc URLs for APIs used by the quickstart
+  DISCOVERY_DOCS: ["https://sheets.googleapis.com/$discovery/rest?version=v4"], // Array of API discovery doc URLs
   SCOPES: "https://www.googleapis.com/auth/spreadsheets", // Authorization scopes required by the API; multiple scopes can be included, separated by spaces.
   SPREADSHEET_ID: "change-this", // Spreadsheet id from Google Sheets URL
   CHORES_RANGE: "Chores!A2:D", // Chores sheet name and range in A1 notation
